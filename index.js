108cd34a6254c71672cc9b350cf6879770392adc
ashfiaKhatun <ashfia1204@gmail.com>
2024-08-20 13:32:40 +0600

    mongodb connection created

--- index.js
+++ index.js
@@ -4,8 +4,26 @@ const app = express();
 
 app.use(cors())
 
+const corsOptions = {
+  origin: ['http://localhost:5173'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
+app.use(express.json());
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.begblt8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://e-commerce:R456Z3qSxrbG8TGs@cluster0.begblt8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,6 +36,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
